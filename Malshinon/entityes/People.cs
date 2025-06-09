d9288a4d9402b54b43397fb49348eb708ec51d5c
Eclipse <Eclips77@users.noreply.github.com>
2025-06-09 11:27:42 +0300

    added folders and first classes

---
+++ Malshinon/entityes/People.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon.interfaces
+{
+    internal class People
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string SecretCode { get; set; }
+        string ManType { get; set; }
+        int NumReports { get; set; }
+        int NumMentions { get; set; }
+
+        public People(string fn, string ln, string sc, string mt,int nr =0,int nm =0)
+        {
+            this.FirstName = fn;
+            this.LastName = ln;
+            this.SecretCode = sc;
+            this.ManType = mt;
+            this.NumReports = nr;
+            this.NumMentions = nm;
+        }
+        public string GetFirstName() => this.FirstName;
+        public string GetLastName() => this.LastName;
+        public string GetSecretCode() => this.SecretCode;
+        public string GetManType() => this.ManType;
+        public int GetNumReports() => this.NumReports;
+        public int GetNumMentions() => this.NumMentions;
+    }
+}
