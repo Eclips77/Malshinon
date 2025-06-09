fb04fb597975c841b01607f6f33c952557256459
Eclipse <Eclips77@users.noreply.github.com>
2025-06-09 11:43:16 +0300

    editing names and start creating dal

---
+++ Malshinon/menus/MalshinManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon.menus
+{
+    internal class MalshinManager
+    {
+        public void ValidPeopleExists(string Fullname)
+        {
+            string[] Names = Fullname.Split(' ');
+
+        }
+    }
+}
